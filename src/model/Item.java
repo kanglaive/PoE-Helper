0b53f1144a19ba6238e0240235e98d0f2bc3e022
Quang Vo <quangdvo@gmail.com>
2017-07-05 16:14:23 -0400

    Commit Then Swap To HTML branch
    Commiting minor changes before swapping to html branch to deal with project documentation.

--- src/model/Item.java
+++ src/model/Item.java
@@ -4,16 +4,23 @@ package model;
  * Created by Kang on 7/4/2017.
  */
 public class Item {
-    private Rarity rarity;
+    private Rarity itemRarity;
+    private String itemName;
 
     /**
      * instantiates item
      * @param string
      */
     public Item(String string) {
+        // split item string by lines
         String[] str_arr = string.split("/n");
+        // try to parse lines
         try {
-            rarity.parseRarity(str_arr[0]);
+            // parse rarity on first line
+            itemRarity.parseRarity(str_arr[0]);
+            // parse name on second line
+            itemName = str_arr[1];
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
