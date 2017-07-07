907b8678395d5882ffc5106d71b9c6b1bff23cbb
Quang Vo <quangdvo@gmail.com>
2017-07-07 08:55:15 -0400

    Removing Unnecessary Files

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -2,16 +2,12 @@ package controller;
 
 
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import model.Item;
